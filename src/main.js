5e9fabd4fd77ebd3af7d8d2cf0a4965d8fa59765
xx <xxx@qq.com>
2020-08-01 19:13:19 +0800

    创建标准工程

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+Vue.use(ElementUI)
 
+Vue.config.productionTip = false
+/* eslint-disable no-new */
 new Vue({
   router,
   store,
